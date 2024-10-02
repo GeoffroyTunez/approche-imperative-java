f7ee4de8550f77bf3cb7a52d4aef9193f6c8d45c
GeoffroyTunez <geoffroytunez@gmail.com>
2024-10-02 15:03:46 +0200

    commit TP2

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,8 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String args[]) {
+        String randomString = "Voici le résultat d'un calcul :\n1+5=6";
+        System.out.println(randomString);
+    }
+}
